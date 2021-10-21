c4aae1e02e73bf49cc47fb8b8853438ae0a93612
namcuongtp2000 <92356571+namcuongtp2000@users.noreply.github.com>
2021-10-21 18:52:49 +0700

    Create process1.c

---
+++ process1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>// standard input / output functions
+
+#include <unistd.h>
+
+#include <time.h>
+
+int main(int argc, char** argv)
+
+{
+
+printf(“\nMa tien trinh dang chay: %d”,(int)getpid());
+
+printf(“\nMa tien trinh cha: %d”,(int)getppid());
+
+while(1)
+
+{
+
+printf(“\nRunning…”);
+
+                      usleep(500);
+
+}
+
+}
